3e1b3ec454a658a5910a5bd97ca727b2811319a8
lzagorski <lukaszzagorski99@gmail.com>
2022-11-03 19:49:17 +0100

    Added basic templates with generic forms

--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from users.forms import UserLoginForm, UserRegisterForm
+
+
+def login_view(request):
+    context = {'form': UserLoginForm()}
+    return render(request, 'login.html', context)
+
+
+def register_view(request):
+    context = {'form': UserRegisterForm()}
+    return render(request, 'register.html', context)
+
+
+def logout_view(request):
+    return redirect('..')
