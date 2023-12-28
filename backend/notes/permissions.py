d57f3597262c5ea8b8fff390cb8ef1a0729424ea
csecluki <lukaszzagorski99@gmail.com>
2023-12-28 14:44:31 +0100

    Update permissions for Note model

--- backend/notes/permissions.py
+++ backend/notes/permissions.py
@@ -4,16 +4,24 @@ from rest_framework import permissions
 class NotePermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        if view.action in ['create', 'retrieve', 'list', 'update', 'partial_update', 'destroy']:
+        if view.action in ['create', 'retrieve', 'destroy']:
             return True
+        if view.action in ['list', 'update', 'partial_update']:
+            return request.user.is_staff
+        if view.action == 'student_notes':
+            return request.user.is_student
         return False
 
     def has_object_permission(self, request, view, obj):
-        if view.action in ['retrieve', 'update', 'partial_update', 'destroy']:
-            return True
+        if view.action == 'retrieve':
+            return request.user == obj.course_group.teacher or request.user == obj.student or request.user.is_staff
+        if view.action == 'destroy':
+            return request.user == obj.course_group.teacher or request.user.is_staff
+        if view.action in ['update', 'partial_update']:
+            return request.user.is_staff
         return False
     
-    def has_permission_for_create(self, request, course_group):
+    def has_permission_for_create_destroy(self, request, course_group):
         return request.user.is_staff or request.user == course_group.teacher
 
 
--- backend/notes/views.py
+++ backend/notes/views.py
@@ -1,9 +1,10 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.exceptions import ValidationError
-from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework import viewsets, status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.exceptions import PermissionDenied
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from .filters import NoteFilter, EndNoteFilter
 from .models import Note, EndNote
@@ -25,9 +26,14 @@ class NoteViewSet(viewsets.ModelViewSet):
 
     def create(self, request, *args, **kwargs):
         course_group = CourseGroup.objects.get(id=request.data.get('course_group', None))
-        if not NotePermission().has_permission_for_create(request, course_group):
+        if not NotePermission().has_permission_for_create_destroy(request, course_group):
             raise PermissionDenied("You don't have permission to create a note for this course group.")
         return super().create(request, *args, **kwargs)
+    
+    @action(methods=['GET'], detail=False)
+    def student_notes(self, request):
+        serializer = self.get_serializer(request.user.student_notes.all(), many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class EndNoteViewSet(viewsets.ModelViewSet):
