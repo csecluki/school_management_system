71f0c077035ae4e42e1a0a707ed66b282533bf4b
csecluki <lukaszzagorski99@gmail.com>
2023-12-29 10:24:57 +0100

    Update populate_notes_note

--- backend/notes/management/commands/populate_notes_note.py
+++ backend/notes/management/commands/populate_notes_note.py
@@ -18,11 +18,18 @@ class Command(PopulateCommand):
             for student in User.objects.filter(groups__name="Students")
             for group in student.enrolled_groups.all()
         ]
+        notes = self.get_note_range(note_range)
         for student, group in student_group:
             for _ in range(int(random.triangular(*distribution))):
                 Note.objects.create(
                     student=student,
                     course_group=group,
-                    value=random.choice(range(*note_range))
+                    value=random.choice(notes)
                 )
         self.stdout.write(self.style.SUCCESS(f'Successfully populated notes_note. '))
+
+    def get_note_range(self, note_range):
+        low, high = note_range
+        r = [i * 0.5 for i in range(low * 2, high * 2 + 1)]
+        r.remove(1.5)
+        return r
